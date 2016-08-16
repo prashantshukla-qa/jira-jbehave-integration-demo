d5951968916ea7af828f4e786fa1899f294bff6c
Prashant Shukla <v-pshukla2@expedia.com>
2016-08-16 09:45:51 +0530

    Refactoring the package structure

---
+++ src/test/java/com/qait/automation/jbehavedemo/getstory/JiraSprintStoryFinder.java
@@ -0,0 +1,53 @@
+package com.qait.automation.jbehavedemo.getstory;
+
+import com.qait.automation.utils.HttpClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ *
+ * @author prashantshukla
+ */
+public class JiraSprintStoryFinder {
+
+    private final String rapidViewPath;
+
+    public JiraSprintStoryFinder(String rapidViewId) {
+        this.rapidViewPath = Constants.JIRA_URL + Constants.JIRA_PATH_RAPIDVIEW + rapidViewId;
+    }
+
+    public List<String> getJiraSprintStories() {
+        List<String> testStories = new ArrayList<>();
+        String sprintJson;
+        try {
+            HttpClient client = new HttpClient();
+            sprintJson = client.getHttpResponse(this.rapidViewPath).getEntity(String.class);
+            JSONObject obj = new JSONObject(sprintJson);
+
+            JSONArray issuesArray = obj.getJSONObject("issuesData").getJSONArray("issues");
+
+            for (int i = 0; i < issuesArray.length(); i++) {
+
+                JSONObject issueJson = new JSONObject(issuesArray.get(i).toString());
+
+                if (Constants.ALLOWED_TYPE.contains(issueJson.getString("typeName").trim())) {
+                    if (Constants.ALLOWED_STATUS.contains(issueJson.getString("statusName").trim())) {
+                        //TODO: System.out.println("The Story: \'" + issueJson.getString("key") + "\' will be tested");
+                        testStories.add(issueJson.getString("key"));
+                    }
+                }
+            }
+        } catch (JSONException ex) {
+            Logger.getLogger(JiraSprintStoryFinder.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+        return testStories;
+    }
+}
---
+++ src/test/java/com/qait/automation/jbehavedemo/tests/JUnitTestRunner.java
@@ -0,0 +1,18 @@
+package com.qait.automation.jbehavedemo.tests;
+
+import com.qait.automation.jbehavedemo.getstory.JiraStoryDownloader;
+import com.qait.automation.utils.FileHandler;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author prashantshukla
+ */
+public class JUnitTestRunner {
+
+    @Test
+    public void testNoJbehavStory() {
+
+    }
+}
